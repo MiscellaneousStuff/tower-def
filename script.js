8fb33578bf11967dc1d1ba1226069239703e992e
Joe Makepeace <joe.makepeace01@gmail.com>
2022-07-17 00:47:39 +0100

    added pathway rendering

--- script.js
+++ script.js
@@ -1,32 +1,64 @@
 const $ = id => { return document.getElementById(id) }
 
-let canvas, ctx;
+let canvas, ctx, game;
 
-const draw = () => {
+const rect = (x, y, w, h) => {
     ctx.fillStyle = "black";
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
-};
-
-const update = () => {
-
+    ctx.fillRect(x, y, w, h);
 }
 
-const loop = () => {
-    // Render update
-    draw();
-    
-    // Game update
-    update();
+class Game {
+    constructor(pathway) {
+        this.pathway = pathway;
+    }
+    draw = () =>  {
+        // Fill background
+        rect(0, 0, canvas.width, canvas.height);
 
-    // Wait until next animation frame
-    window.requestAnimationFrame(loop);
-};
+        // Fill path
+        ctx.beginPath();
+        ctx.strokeStyle = "white";
+        for (let i=0; i<this.pathway.length-1; i++) {
+            let a = this.pathway[i];
+            let b = this.pathway[i+1];
+            ctx.moveTo(a[0], a[1]);
+            ctx.lineTo(b[0], b[1]);
+        }
+        ctx.stroke();
+    }
+    update = () => {
+
+    };
+    loop = () => {
+        // Render update
+        this.draw();
+        
+        // Game update
+        this.update();
+    }
+}
 
 window.onload = () => {
+    // Init game variables
+    game = new Game([
+        [0, 240],
+        [150, 240],
+        [150, 360],
+        [300, 360],
+        [300, 240],
+        [640, 240]
+    ]);
+
     // Init canvas variables
     canvas = $("canvas");
     ctx = canvas.getContext("2d");
 
     // Init game loop
-    window.requestAnimationFrame(loop);
+    window.requestAnimationFrame(() => {
+        // Execute game loop
+        game.loop();
+
+        // Wait until next animation frame
+        window.requestAnimationFrame(game.loop);
+    });
 }
